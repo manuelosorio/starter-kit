efd1fc7490ab0ee84028c4fdfe8c51046a2958df
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2019-06-15 09:12:37 -0400

    Add image minifier

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Starter Kit used for my future projects
 ## TODO
 * Add processing for: 
-    - Images
-    - Javascript
-    - Pug
-    - Sass
+    - [x] ~~Images~~
+    - [ ] JavaScript
+    - [x] ~~Pug~~
+    - [x] ~~Sass~~
 * Create an simple CMD tool to make new project using this repository
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require("gulp"),
+    clean = require("gulp-clean"),
     sass = require("gulp-sass"),
     postcss = require("gulp-postcss"),
     pug = require("gulp-pug"),
+    imagemin = require('gulp-imagemin'),
+    newer = require('gulp-newer'),
+    plumber = require('gulp-plumber'),
     autoprefixer = require("autoprefixer"),
     cssnano = require("cssnano"),
     sourcemaps = require("gulp-sourcemaps"),
@@ -9,21 +13,25 @@ var gulp = require("gulp"),
 
 var paths ={
   styles: {
-      src: "src/assets/css/**/*.sass",
+      src: "src/assets/css/**/*.{sass,scss}",
       dest: "_dist/css"
   },
   html: {
     src: "src/**/*.pug",
     dest: "_dist/"
+  },
+  images: {
+    src: "src/assets/images/**/*.{jpg,png,svg}",
+    dest: "_dist/images"
   }
 }
 function style() {
   return gulp
     .src(paths.styles.src)
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass({ outputStyle: "expanded" }))
     .on("error", sass.logError)
-    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(browserSync.stream());
@@ -32,13 +40,41 @@ function html() {
   return gulp
     .src(paths.html.src)
     .pipe(pug({
+      pretty: true
     }))
     .pipe(gulp.dest(paths.html.dest))
 }
+
+function images () {
+  return gulp
+    .src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream());
+}
+
 function reload() {
   browserSync.reload();
 }
-
+function cleanDist() {
+  return gulp.src('./_dist', {allowEmpty:true})
+        .pipe(clean())
+}
 function watch() {
   browserSync.init({
     server: {
@@ -46,12 +82,15 @@ function watch() {
     }
   });
   gulp.watch(paths.styles.src, style);
+  gulp.watch(paths.images.src, images);
   gulp.watch(paths.html.src, html).on('change', browserSync.reload);
 }
 exports.watch = watch
 exports.style = style
+exports.images = images
 exports.html = html
+exports.cleanDist = cleanDist
 
-var build = gulp.parallel([html, style], watch);
+var build = gulp.parallel([cleanDist,[html, style, images]], watch);
 
 gulp.task('default', build)
